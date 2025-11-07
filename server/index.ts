c8e900fbcd47d09e1eec2fe284ed033801216585
zrotrasukha <zrotrasukha@gmail.com>
2025-11-07 19:47:25 +0530

    Introduce websocket functionality

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+const serve = Bun.serve({
+    port: 3000,
+    routes: {
+        "/api/status": new Response("OK")
+    },
+    fetch(req, server) {
+        if (server.upgrade(req)) {
+            return;
+        }
+        return new Response("Hello, Bun!");
+    },
+    websocket: {
+        open(ws) {
+            ws.send("Client connected");
+        }, // a socket is opened
+        message(ws, message) {
+            ws.send(`Echo: ${message}`);
+        }, // a message is received
+        close(ws, code, message) { }, // a socket is closed
+        drain(ws) { }, // the socket is ready to receive more data
+    }
+})
+
+console.log(`Server running at http://localhost:${serve.port}/`);
+
