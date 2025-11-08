2ae853a5f3f1e6e9ee003e670b3436d236e5da43
zrotrasukha <zrotrasukha@gmail.com>
2025-11-08 05:58:59 +0530

    Refactor project structure by removing unused App component and styles, and updating configuration files for improved organization and clarity

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        react(),
+        tailwindcss()
+
+    ],
+    resolve: {
+        alias: {
+            '@': path.dirname(__dirname + '/src/'),
+        }
+    }
 })
